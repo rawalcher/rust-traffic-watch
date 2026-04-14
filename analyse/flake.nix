8dd0c8b9c36256b1b049067612e6704cc63bd4f7
rawalcher <raphael@walcher.dev>
2026-04-14 15:50:46 +0200

    feat: add direnv for rstudio

---
+++ analyse/flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { nixpkgs, ... }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.permittedInsecurePackages = [ "electron-38.8.4" ];
+    };
+    rEnv = pkgs.rstudioWrapper.override {
+      packages = with pkgs.rPackages; [ tidyverse data_table knitr rmarkdown ];
+    };
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [ rEnv pkgs.quarto ];
+    };
+  };
+}
+
